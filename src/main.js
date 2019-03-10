6c809b9b47a7f2c85a7d4145f716df07eaa826fc
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-03-10 20:47:47 -0300

    Add calculator

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => (num2 === 0 ? 'Não é possível divisão por zero!' : num1 / num2);
+
+export {
+  sum,
+  sub,
+  mult,
+  div,
+};
